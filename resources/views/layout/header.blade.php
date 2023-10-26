0f08891558f14a19d02911a8c60bbdced4324128
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-26 15:45:12 +0200

    add header, app and home structure

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+
+    <div class="container">
+        <nav class="navbar py-0">
+            <a class="navbar-brand" href="#">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC Logo">
+            </a>
+            <div id="page" class="d-flex gap-4">
+                <a class="py-5" href="##">link</a>
+                <a class="py-5" href="##">link</a>
+                <a class="py-5" href="##">link</a>
+                <a class="py-5" href="##">link</a>
+                <a class="py-5" href="##">link</a>
+            </div>
+        </nav>
+    </div>
+</header>
