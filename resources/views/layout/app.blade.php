0691837275618a43d8da22a874a02e5f97c4f58e
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-26 15:53:08 +0200

    add logo title

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>DC Comics</title>
+    <title>@yield('page-title', 'DC Comics')</title>
+
+    <!-- Logo Title -->
+    <link rel="icon" href="{{Vite::asset('resources/img/dc-logo.png')}}" type="image/png">
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
